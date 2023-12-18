53921078100f58d1b2fac35b1f6b5c13bab0eff2
James Quintas <43793626+Quintilium@users.noreply.github.com>
2023-12-18 16:49:19 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,244 @@
+import { useCallback } from "react";
+import styles from "./index.module.css";
+
+const Main3 = () => {
+  const onFrameContainer2Click = useCallback(() => {
+    // Please sync "mailchimp sign up" to the project
+  }, []);
+
+  return (
+    <div className={styles.main3}>
+      <div className={styles.main3Child} />
+      <div className={styles.main3Item} />
+      <div className={styles.main3Inner} />
+      <div className={styles.ellipseDiv} />
+      <div className={styles.main3Child1} />
+      <div className={styles.navbar}>
+        <div className={styles.navbarChild} />
+        <div className={styles.navbarItem} />
+        <div className={styles.navbarInner} />
+        <b className={styles.pixeloasis}>PixelOasis</b>
+        <div className={styles.homeParent}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.home}>Merch</div>
+          <div className={styles.home}>Tournaments</div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.rectangleParent}>
+          <div className={styles.groupChild} />
+          <div className={styles.groupItem} />
+          <div className={styles.groupInner} />
+        </div>
+        <b className={styles.po}>PO</b>
+        <div className={styles.homeGroup}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.home}>Merch</div>
+          <div className={styles.home}>Tournaments</div>
+          <div className={styles.home}>Sign In</div>
+        </div>
+      </div>
+      <div className={styles.hero}>
+        <b className={styles.free1MonthTrial}>
+          FREE 1-month trial to new sign up members!
+        </b>
+        <div className={styles.signUpFor}>
+          Sign up for free and gain access to all games and be part of our
+          community.
+        </div>
+        <div className={styles.signUpWrapper} onClick={onFrameContainer2Click}>
+          <b className={styles.signUp}>Sign Up</b>
+        </div>
+        <img
+          className={styles.dalle20231208011529A}
+          alt=""
+          src="/dalle-20231208-011529--a-cozy-living-room-scene-depicting-three-happy-friends-of-different-descents-playing-video-games-together-1@2x.png"
+        />
+      </div>
+      <div className={styles.community}>
+        <div className={styles.communityChild} />
+        <div className={styles.communityItem} />
+        <div className={styles.communityInner} />
+        <b className={styles.weRunBy}>We run by our community</b>
+        <div className={styles.throughSpecialEvents}>
+          Through special events, tournaments, and movie nights, our community
+          welcomes all members.
+        </div>
+        <img
+          className={styles.communityIcon61}
+          alt=""
+          src="/communityicon6-1@2x.png"
+        />
+      </div>
+      <div className={styles.tournaments2}>
+        <b className={styles.upcomingTournaments}>UPCOMING TOURNAMENTS</b>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.classicArcade}>Classic Arcade</b>
+          <div className={styles.dec1stGamesContainer}>
+            <p className={styles.dec1st}>Dec. 1st</p>
+            <p className={styles.dec1st}>&nbsp;</p>
+            <p className={styles.dec1st}>
+              Games we’ll play: Pac-Man, Donkey Kong, Space Invaders, Galaga
+            </p>
+          </div>
+        </div>
+        <div className={styles.rectangleContainer}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.segaNight}>Sega Night</b>
+          <div className={styles.dec1stGamesContainer}>
+            <p className={styles.dec1st}>Dec. 15th</p>
+            <p className={styles.dec1st}>&nbsp;</p>
+            <p className={styles.dec1st}>
+              Games we’ll play: Sonic the Hedgehog, Streets of Rage, Golden Axe,
+              Mortal Kombat
+            </p>
+          </div>
+        </div>
+        <div className={styles.groupDiv}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.atariTime}>Atari Time</b>
+          <div className={styles.dec1stGamesContainer}>
+            <p className={styles.dec1st}>Dec. 22nd</p>
+            <p className={styles.dec1st}>&nbsp;</p>
+            <p className={styles.dec1st}>
+              Games we’ll play: Space Invaders, Missile Command, Asteroids,
+              Pitfall
+            </p>
+          </div>
+        </div>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.dec8thGamesContainer}>
+            <p className={styles.dec1st}>Dec. 8th</p>
+            <p className={styles.dec1st}>&nbsp;</p>
+            <p className={styles.dec1st}>
+              Games we’ll play: Super Mario Bros, The Legend of Zelda, Street
+              Fighter II
+            </p>
+          </div>
+          <b className={styles.nesMarathon}>NES Marathon</b>
+        </div>
+      </div>
+      <div className={styles.merch2}>
+        <b className={styles.exclusiveMerch}>EXCLUSIVE MERCH</b>
+        <div className={styles.buyNow80sRetroGamingTshirWrapper}>
+          <img
+            className={styles.buyNow80sRetroGamingTshirIcon}
+            alt=""
+            src="/buynow80sretrogamingtshirtcabinetvideogamegiftforgeeksblackunisextshirt-1@2x.png"
+          />
+        </div>
+        <img
+          className={styles.atarishotGrande1Icon}
+          alt=""
+          src="/atarishot-grande-1@2x.png"
+        />
+        <img
+          className={styles.o80q5ej3l1Icon}
+          alt=""
+          src="/61o80q5ej3l-1@2x.png"
+        />
+        <img
+          className={styles.a1nynisnpelAcCla21402000Icon}
+          alt=""
+          src="/a1nynisnpel-ac-cla21402000a1h4k3rtplpng002140200000002140020000-uy1000--1@2x.png"
+        />
+        <img
+          className={styles.levelonedonkeykongshirt80steesIcon}
+          alt=""
+          src="/607775levelonedonkeykongshirt80stees02c8f944d19c268109b4f0422d3a12f66-1@2x.png"
+        />
+      </div>
+      <div className={styles.experience}>
+        <b className={styles.pricing}>PRICING</b>
+        <div className={styles.rectangleParent2}>
+          <div className={styles.groupChild4} />
+          <div className={styles.monthWrapper}>
+            <b className={styles.signUp}>$0.99/month</b>
+          </div>
+          <b className={styles.player}>PLAYER</b>
+          <div className={styles.accessToOur}>
+            ✔️ Access to our arcade consoles
+          </div>
+        </div>
+        <div className={styles.experienceInner}>
+          <div className={styles.groupChild5} />
+          <b className={styles.master}>MASTER</b>
+          <div className={styles.accessToOur}>
+            <p className={styles.dec1st}>✔️ Access to our arcade consoles</p>
+            <p className={styles.dec1st}>
+              ✔ Access to tournaments every friday
+            </p>
+            <p className={styles.dec1st}>✔ Community events</p>
+            <p className={styles.dec1st}>{`✔ Exclusive merch & giveaways`}</p>
+          </div>
+          <div className={styles.monthContainer}>
+            <b className={styles.signUp}>$30/month</b>
+          </div>
+        </div>
+        <div className={styles.rectangleParent4}>
+          <div className={styles.groupChild5} />
+          <div className={styles.accessToOurContainer1}>
+            <p className={styles.dec1st}>✔️ Access to our arcade consoles</p>
+            <p className={styles.dec1st}>
+              ✔ Access to tournaments every friday
+            </p>
+            <p className={styles.dec1st}>✔ Community events</p>
+          </div>
+          <b className={styles.member}>MEMBER</b>
+          <div className={styles.monthContainer}>
+            <b className={styles.signUp}>$5/month</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.experience1}>
+        <b className={styles.testimonials}>TESTIMONIALS</b>
+        <div className={styles.rectangleParent5}>
+          <div className={styles.groupChild4} />
+          <b className={styles.julio}>JULIO</b>
+        </div>
+        <div className={styles.experienceInner}>
+          <div className={styles.groupChild5} />
+        </div>
+        <div className={styles.rectangleParent6}>
+          <div className={styles.groupChild9} />
+          <img
+            className={styles.dalle20231209173138A}
+            alt=""
+            src="/dalle-20231209-173138--a-closeup-portrait-of-a-young-adult-south-asian-male-gamer-smiling-he-has-short-black-hair-and-is-wearing-a-casual-tshirt-suitable-for-gaming-1@2x.png"
+          />
+        </div>
+      </div>
+      <div className={styles.alexParent}>
+        <b className={styles.alex}>ALEX</b>
+        <div className={styles.iDidntKnow}>
+          “I didn’t know about this until a friend told me about it, my life
+          changed.”
+        </div>
+      </div>
+      <div className={styles.markParent}>
+        <b className={styles.alex}>MARK</b>
+        <div className={styles.iDidntKnow}>
+          “This is my way to connect with a community that values the same
+          things I do.”
+        </div>
+      </div>
+      <img
+        className={styles.dalle20231209173059A}
+        alt=""
+        src="/dalle-20231209-173059--a-closeup-portrait-of-a-young-adult-black-male-gamer-smiling-he-has-short-curly-hair-and-is-wearing-a-graphic-tee-1@2x.png"
+      />
+      <img
+        className={styles.dalle20231209173050A}
+        alt=""
+        src="/dalle-20231209-173050--a-closeup-portrait-of-a-young-adult-hispanic-female-gamer-smiling-she-has-long-wavy-brown-hair-and-is-wearing-a-comfortable-hoodie-1@2x.png"
+      />
+      <div className={styles.ofAllSubscriptions}>
+        “Of all subscriptions, this one is the most valuable one I’ve ever had.”
+      </div>
+    </div>
+  );
+};
+
+export default Main3;
